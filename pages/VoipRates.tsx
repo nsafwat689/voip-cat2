6c33c2d4a0736acbc0e75a2e5020cb06fbbf98e0
nsafwat689 <n.safwat689@gmail.com>
2026-03-27 01:42:43 +0200

    Apply voip-catfinal Network Plans theme to VoIP Rates page
    - Changed outer div background from hardcoded gradient to bg-background
    - Added bg-background to section element for consistent theme support
    - Matches PlansSection.tsx styling from voip-catfinal

--- pages/VoipRates.tsx
+++ pages/VoipRates.tsx
@@ -65,10 +65,10 @@ export default function VoipRates() {
   ];
 
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900">
+    <div className="min-h-screen flex flex-col bg-background">
       <Header />
       <main className="flex-grow">
-        <section id="rates" className="py-20 md:py-32 relative">
+        <section id="rates" className="py-20 md:py-32 bg-background relative">
           {/* Background accent */}
           <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[url('https://www.transparenttextures.com/patterns/carbon-fibre.png')] opacity-5 -z-10"></div>
 
